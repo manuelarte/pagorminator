45e8494e22ff49940dd6b405080437dae6106dc0
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2026-08-08 22:59:38 +0200

    wip

--- pagegeneric/models.go
+++ pagegeneric/models.go
@@ -1,12 +1,17 @@
 package pagegeneric
 
-//nolint:gochecknoglobals // global variables to specify why there is no next page.
-var (
-	NoNextPage                 = NextPossible(false)
-	NoTotalElements            = NextPossible(false)
+const (
+	// NoNextPage is a constant that represents that there is no next page.
+	NoNextPage = NextPossible(false)
+	// NoTotalElements is a constant that represents that the total elements are not set,
+	// so we can't know whether there is a next page.
+	NoTotalElements = NextPossible(false)
+	// PreviousCursorValuesNotSet is a constant that represents that the previous cursor values
+	// are not set, so we can't know whether there is a next page.
 	PreviousCursorValuesNotSet = NextPossible(false)
 )
 
 type (
+	// NextPossible represents whether the next page is available.
 	NextPossible bool
 )
