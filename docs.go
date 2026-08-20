7fd297a75b6f14fe9e378f10a78c045c6effaa81
manuelarte <manueldoncelmartos@gmail.com>
2026-08-20 22:58:16 +0200

    wip

---
+++ docs.go
@@ -0,0 +1,4 @@
+// Package pagorminator providers pagination utilities for GORM.
+// It provides two types of pagination: cursor-based and page-based.
+// It also provides a generic interface for pagination that can be used to implement custom pagination strategies.
+package pagorminator
